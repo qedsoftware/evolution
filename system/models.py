9c2bfa17a155705f44b3266e8a923d10f715ab6a
Stanisław Barzowski <sbarzowski@qed.ai>
2016-04-26 13:13:34 +0200

    Move markdown logic out of system/models.py

--- system/models.py
+++ system/models.py
@@ -1,7 +1,5 @@
-import html
-import markdown
-import bleach
 import reprlib
+import html
 from datetime import timedelta
 
 from django.db import models, transaction
@@ -13,6 +11,8 @@ from django.contrib.sites.models import Site
 from django.core.exceptions import ValidationError
 from allauth.account.models import EmailAddress
 from allauth.utils import build_absolute_uri
+
+from .markdown import markdown_to_html
 
 SOURCE_LANGUAGES = (
     ('html', 'HTML'),
@@ -49,77 +49,6 @@ class Post(models.Model):
     def __str__(self):
         return str(self.id) + ' (' + self.source_lang + ') ' + \
             reprlib.repr(self.source)
-
-
-MARKDOWN_ALLOWED_TAGS = [
-    'a',
-    'abbr',
-    'acronym',
-    'b',
-    'br',
-    'blockquote',
-    'code',
-    'div',
-    'em',
-    'i',
-    'li',
-    'ol',
-    'pre',
-    'span',
-    'strong',
-    'ul',
-    'p',
-    'table',
-    'thead',
-    'tr',
-    'th',
-    'td',
-    'tbody',
-    'tfoot',
-    'h1',
-    'h2',
-    'h3',
-    'h4',
-    'h5',
-    'h6'
-    # 'blockquote', # TODO support in css
-    #  definition lists - TODO support them in css
-    # 'dl',
-    # 'dt',
-    # 'dd'
-]
-
-ALLOWED_CSS_CLASSES = ['highlight', 'bp', 'c', 'c1', 'cm', 'cp', 'cs', 'err',
-     'gd', 'ge', 'gh', 'gi', 'go', 'gp', 'gr', 'gs', 'gt', 'gu', 'hll', 'il',
-      'k', 'kc', 'kd', 'kn', 'kp', 'kr', 'kt', 'm', 'mf', 'mh', 'mi', 'mo',
-      'na', 'nb', 'nc', 'nd', 'ne', 'nf', 'ni', 'nl', 'nn', 'no', 'nt', 'nv',
-      'o', 'ow', 's', 's1', 's2', 'sb', 'sc', 'sd', 'se', 'sh', 'si', 'sr',
-      'ss', 'sx', 'vc', 'vg', 'vi', 'w']
-
-
-def safe_class(name, value):
-    return name == "class" and value in ALLOWED_CSS_CLASSES
-
-
-MARKDOWN_ALLOWED_ATTRIBUTES = {
-    'abbr': ['title'],
-    'acronym': ['title'],
-    'a': ['href', 'title'],
-    'span': safe_class,
-    'div': safe_class,
-    'tr': ['align'],
-    'th': ['align'],
-    'td': ['align'],
-    'tbody': ['align'],
-    'tfoot': ['align']
-}
-
-
-def markdown_to_html(source):
-    markdown_html = markdown.markdown(source, extensions=['mdx_partial_gfm'])
-    return bleach.clean(markdown_html,
-        tags=MARKDOWN_ALLOWED_TAGS,
-        attributes=MARKDOWN_ALLOWED_ATTRIBUTES)
 
 
 post_source_processors = {
